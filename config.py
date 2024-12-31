5d32a1e6cf265bfb08f87369ca81032e355e6349
ivymmurithi <ivymurithi@gmail.com>
2024-12-31 16:07:29 +0300

    (feat): Database and models, User

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+import os
 from decouple import config
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='sqlite:///' + os.path.join(basedir, 'app.db'))
