7d6e6473b503041347fbe68cde5779adacd6ed82
ivymmurithi <ivymurithi@gmail.com>
2025-01-13 21:34:38 +0300

    (feat): get following posts

--- app/models.py
+++ app/models.py
@@ -70,6 +70,17 @@ class User(UserMixin, db.Model):
         query = sa.select(sa.func.count()).select_from(
             self.following.select().subquery())
         return db.session.scalar(query)
+    
+    def following_posts(self, user):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        return (
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower))
+            .where(Follower.id == self.id)
+            .order_by(Post.timestamp.desc())
+        )
 
 
 class Post(db.Model):
